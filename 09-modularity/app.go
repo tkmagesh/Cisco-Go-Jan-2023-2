a909fcd9551a2efd5240eaa844d4c44569db4ef9
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-24 11:27:04 +0530

    using 3rd party packages

--- 09-modularity/app.go
+++ 09-modularity/app.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 
-	_ "github.com/tkmagesh/Cisco-Go-Jan-2023-2/09-modularity/calculator"
+	"github.com/tkmagesh/Cisco-Go-Jan-2023-2/09-modularity/calculator"
+
+	// _ "github.com/tkmagesh/Cisco-Go-Jan-2023-2/09-modularity/calculator"
+
+	"github.com/fatih/color"
 	app_utils "github.com/tkmagesh/Cisco-Go-Jan-2023-2/09-modularity/calculator/utils"
 )
 
 func main() {
-	fmt.Println("app invoked")
+	// fmt.Println("app invoked")
 
-	/*
-		fmt.Println(calculator.Add(100, 200))
-		fmt.Println(calculator.Subtract(100, 200))
-		fmt.Println("Op count :", calculator.OpCount())
-	*/
+	color.Red("app invoked")
+
+	fmt.Println(calculator.Add(100, 200))
+	fmt.Println(calculator.Subtract(100, 200))
+	fmt.Println("Op count :", calculator.OpCount())
 
 	// fmt.Println("is 21 even ?:", utils.IsEven(21))
 	fmt.Println("is 21 even ?:", app_utils.IsEven(21))
