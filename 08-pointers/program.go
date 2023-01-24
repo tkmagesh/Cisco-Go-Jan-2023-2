9200da38187cdfe62a5e4b13ecdb4effaed0a4b4
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-24 09:28:04 +0530

    updates

--- 08-pointers/program.go
+++ 08-pointers/program.go
@@ -16,11 +16,21 @@ func main() {
 
 	fmt.Println(no == *(&no))
 
-	fmt.Println("Before incrementing, no = ", no)
-	increment(no)
-	fmt.Println("After incrementing, no = ", no)
+	fmt.Println("Before incrementing, no = ", no) // 100
+	increment(&no)
+	fmt.Println("After incrementing, no = ", no) // 101
+
+	x, y := 100, 200
+	fmt.Printf("Before swapping, x = %d and y = %d\n", x, y)
+	swap(&x, &y)
+	fmt.Printf("After swapping, x = %d and y = %d\n", x, y)
 }
 
-func increment(no int) /* no return result */ {
-	no++
+func increment(n *int) /* no return result */ {
+	fmt.Println("&n =", n)
+	*n++
+}
+
+func swap(n1, n2 *int) /* no return result */ {
+	*n1, *n2 = *n2, *n1
 }
