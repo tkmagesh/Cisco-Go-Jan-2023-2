2f875429d6db286bb69aa0a2e79b21b0bc638a3a
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-24 11:44:29 +0530

    arrays

---
+++ 10-collections/01-arrays.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nos [5]int
+	// var nos [5]int = [5]int{3, 1, 4, 2, 5}
+	// var nos [5]int = [5]int{3, 1, 4}
+	// var nos = [5]int{3, 1, 4, 2, 5}
+	nos := [5]int{3, 1, 4, 2, 5}
+	fmt.Println(nos)
+
+	fmt.Println("Iterating an array (using indexer)")
+	for idx := 0; idx < 5; idx++ {
+		fmt.Printf("nos[%d] = %d\n", idx, nos[idx])
+	}
+
+	fmt.Println("Iterating an array (using range)")
+	for idx, val := range nos {
+		fmt.Printf("nos[%d] = %d\n", idx, val)
+		// fmt.Println(val)
+	}
+
+	list1 := [3]string{"Pen", "Pencil", "Marker"}
+	list2 := [3]string{"Pen", "Pencil", "Marker"}
+	fmt.Printf("&list1 = %p, &list2 = %p\n", &list1, &list2)
+
+	fmt.Println(list1 == list2)
+
+	n1 := 100
+	n2 := 100
+	fmt.Printf("&n1 = %p, &n2 = %p\n", &n1, &n2)
+	fmt.Println(n1 == n2)
+
+	sort(nos)
+	fmt.Println("After sorting, nos = ", nos)
+}
+
+func sort() /* do not return anything */ {
+
+}
