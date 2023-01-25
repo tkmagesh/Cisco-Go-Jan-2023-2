d8e052cd60b934d98ec630b36e0f5dc82eb41484
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-25 09:47:20 +0530

    interface composition

--- 13-interfaces/program.go
+++ 13-interfaces/program.go
@@ -13,6 +13,10 @@ func (c Circle) Area() float32 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+func (c Circle) Perimeter() float32 {
+	return 2 * math.Pi * c.Radius
+}
+
 type Rectangle struct {
 	Length float32
 	Width  float32
@@ -20,6 +24,10 @@ type Rectangle struct {
 
 func (r Rectangle) Area() float32 {
 	return r.Length * r.Width
+}
+
+func (r Rectangle) Perimeter() float32 {
+	return 2 * (r.Length + r.Width)
 }
 
 /* utility functions */
@@ -31,14 +39,39 @@ func PrintArea(x AreaFinder) {
 	fmt.Println("Area :", x.Area())
 }
 
+type PerimeterFinder interface {
+	Perimeter() float32
+}
+
+func PrintPerimeter(x PerimeterFinder) {
+	fmt.Println("Perimeter :", x.Perimeter())
+}
+
+//interface composition
+type Shape interface {
+	AreaFinder
+	PerimeterFinder
+}
+
+func PrintStats(x Shape) {
+	PrintArea(x)
+	PrintPerimeter(x)
+}
+
 func main() {
 	c := Circle{Radius: 12}
 	// fmt.Println("Area :", c.Area())
-	PrintArea(c)
-	PrintPerimeter(c) // (2 * pi * r)
+	/*
+		PrintArea(c)
+		PrintPerimeter(c) // (2 * pi * r)
+	*/
+	PrintStats(c)
 
 	r := Rectangle{Length: 10, Width: 12}
 	// fmt.Println("Area :", r.Area())
-	PrintArea(r)
-	PrintPerimeter(r) // (2 * (length + width))
+	/*
+		PrintArea(r)
+		PrintPerimeter(r) // (2 * (length + width))
+	*/
+	PrintStats(r)
 }
